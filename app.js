1294403ce3df15723f1a8914aa6f8f892f81e2f7
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-08-11 17:30:26 -0500

    casi-MVP

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/anuncios', {
+mongoose.connect('mongodb+srv://JairoProDev:isOgCEALmpQsfA86@cluster0.cykdeq5.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Define el esquema del anuncio
 const anuncioSchema = new mongoose.Schema({
-  // Define los campos aquí
+  titulo: String,
+  descripcion: String,
 });
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
-// Configura rutas y controladores aquí
+app.use(express.json());
+
+app.post('/api/anuncios', async (req, res) => {
+  try {
+    const nuevoAnuncio = new Anuncio(req.body);
+    await nuevoAnuncio.save();
+    res.status(201).json({ mensaje: 'Anuncio creado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al crear el anuncio' });
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+  }); */
+
+app.get('/api/anuncios', async (req, res) => {
+  try {
+    const anuncios = await Anuncio.find();
+    res.status(200).json(anuncios);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los anuncios' });
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor en línea en el puerto ${PORT}`);
+  console.log(`Servidor en línea en http://localhost:${PORT}`);
 });
--- public/script.js
+++ public/script.js
@@ -3,5 +3,86 @@ document.addEventListener('DOMContentLoaded', () => {
     const anunciosList = document.getElementById('anuncios-list');
   
     // Lógica JavaScript aquí para enviar y obtener anuncios
+    
+  anuncioForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const titulo = e.target.titulo.value;
+    const descripcion = e.target.descripcion.value;
+    const nuevoAnuncio = { titulo, descripcion };
+
+    try {
+      const respuesta = await fetch('/api/anuncios', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(nuevoAnuncio),
+      });
+
+      if (respuesta.ok) {
+        console.log('Anuncio creado exitosamente');
+        anuncioForm.reset();
+        obtenerAnuncios();
+      } else {
+        console.error('Error al crear el anuncio');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
   });
+
+  async function obtenerAnuncios() {
+    try {
+      const respuesta = await fetch('/api/anuncios');
+      const anuncios = await respuesta.json();
+
+      anunciosList.innerHTML = '';
+      anuncios.forEach((anuncio) => {
+        const li = document.createElement('li');
+        li.textContent = `${anuncio.titulo}: ${anuncio.descripcion}`;
+        anunciosList.appendChild(li);
+      });
+    } catch (error) {
+      console.error('Error al obtener los anuncios', error);
+    }
+  }
+
+  obtenerAnuncios();
+  
+  // Nueva lógica para mostrar anuncios con botones de editar y eliminar
+  function mostrarAnunciosConBotones(anuncios) {
+    anunciosList.innerHTML = '';
+    anuncios.forEach((anuncio) => {
+      const li = document.createElement('li');
+      li.className = 'anuncio-card';
+      
+      const titulo = document.createElement('h3');
+      titulo.className = 'anuncio-titulo';
+      titulo.textContent = anuncio.titulo;
+      
+      const descripcion = document.createElement('p');
+      descripcion.className = 'anuncio-descripcion';
+      descripcion.textContent = anuncio.descripcion;
+      
+      const buttonsDiv = document.createElement('div');
+      buttonsDiv.className = 'anuncio-buttons';
+      
+      const editarButton = document.createElement('button');
+      editarButton.textContent = 'Editar';
+      
+      const eliminarButton = document.createElement('button');
+      eliminarButton.textContent = 'Eliminar';
+      
+      buttonsDiv.appendChild(editarButton);
+      buttonsDiv.appendChild(eliminarButton);
+      
+      li.appendChild(titulo);
+      li.appendChild(descripcion);
+      li.appendChild(buttonsDiv);
+      
+      anunciosList.appendChild(li);
+    });
+  }
+
+});
   
