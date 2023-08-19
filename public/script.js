c59eba2be43e0e090d5e125526758325be371bbc
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-08-18 22:59:56 -0500

    funcional but bad designs

--- public/script.js
+++ public/script.js
@@ -1,123 +1,131 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const anuncioForm = document.getElementById('anuncio-form');
-    const anunciosList = document.getElementById('anuncios-list');
+  const anuncioForm = document.querySelector('.form-column'); // Cambio en esta línea
+  const anunciosList = document.getElementById('anuncios-list');
+  const toggleFormButton = document.getElementById("toggle-form-button");
+  const formColumn = document.querySelector(".form-column");
 
-    const toggleFormButton = document.getElementById("toggle-form-button");
-    const formColumn = document.querySelector(".form-column");
-  
-    toggleFormButton.addEventListener("click", function() {
-        formColumn.classList.toggle("show-form");
-    });
+  toggleFormButton.addEventListener("click", function () {
+    anuncioForm.classList.toggle("show-form"); // Uso de la variable anuncioForm
+  });
 
-  
-    // Agregar evento para enviar nuevos anuncios
-    anuncioForm.addEventListener('submit', async (e) => {
+
+  // Agregar evento para enviar nuevos anuncios
+  anuncioForm.addEventListener('submit', async (e) => {
       e.preventDefault();
-      const tipo = e.target.elements.tipo.value;
-      const titulo = e.target.elements.titulo.value;
-      const descripcion = e.target.elements.descripcion.value;
-      const nombreAnunciante = e.target.elements.nombreAnunciante.value;
-      const numerosContacto = e.target.elements.numerosContacto.value.split(',');
-      const categoria = e.target.elements.categoria.value;
-      const ubicacion = e.target.elements.ubicacion.value;
-      const monto = e.target.elements.monto.value === '' ? null : parseFloat(e.target.elements.monto.value);
-      const imagenes = e.target.elements.imagenes.value.split(',');
-      const nuevoAnuncio = {
-        tipo,
-        titulo,
-        descripcion,
-        nombreAnunciante,
-        numerosContacto,
-        categoria,
-        ubicacion,
-        monto,
-        imagenes
-      };
-  
+      const nuevoAnuncio = obtenerDatosFormulario(e.target);
       try {
-        const respuesta = await fetch('/api/anuncios', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(nuevoAnuncio),
-        });
-  
-        if (respuesta.ok) {
-          console.log('Anuncio creado exitosamente');
-          anuncioForm.reset();
-          obtenerAnuncios();
-        } else {
-          console.error('Error al crear el anuncio');
-        }
+          const respuesta = await fetch('/api/anuncios', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(nuevoAnuncio),
+          });
+
+          if (respuesta.ok) {
+              console.log('Anuncio creado exitosamente');
+              e.target  .reset();
+              agregarAnuncioAlPrincipio(nuevoAnuncio);
+          } else {
+              console.error('Error al crear el anuncio');
+          }
       } catch (error) {
-        console.error('Error de red:', error);
+          console.error('Error de red:', error);
       }
-    });
-  
-    // Obtener y mostrar anuncios al cargar la página
-    async function obtenerAnuncios() {
+  });
+
+  // Obtener y mostrar anuncios al cargar la página
+  async function obtenerAnuncios() {
       try {
-        const respuesta = await fetch('/api/anuncios');
-        const anuncios = await respuesta.json();
-        mostrarAnunciosConBotones(anuncios); // Llamada a la función de mostrar anuncios con botones
+          const respuesta = await fetch('/api/anuncios');
+          const anuncios = await respuesta.json();
+          mostrarAnuncios(anuncios);
       } catch (error) {
-        console.error('Error al obtener los anuncios', error);
+          console.error('Error al obtener los anuncios', error);
       }
-    }
-  
-    // Mostrar anuncios con botones de editar y eliminar
-    function mostrarAnunciosConBotones(anuncios) {
+  }
+
+  // Mostrar anuncios
+  function mostrarAnuncios(anuncios) {
       anunciosList.innerHTML = '';
       anuncios.forEach((anuncio) => {
-        const li = document.createElement('li');
-        li.className = 'anuncio-card';
-        
-        const titulo = document.createElement('h3');
-        titulo.className = 'anuncio-titulo';
-        titulo.textContent = anuncio.titulo;
-        
-        const descripcion = document.createElement('p');
-        descripcion.className = 'anuncio-descripcion';
-        descripcion.textContent = anuncio.descripcion;
-        
-        const categoria = document.createElement('p');
-        categoria.textContent = `Categoría: ${anuncio.categoria}`;
-  
-        const ubicacion = document.createElement('p');
-        ubicacion.textContent = `Ubicación: ${anuncio.ubicacion}`;
-  
-        const monto = document.createElement('p');
-        monto.textContent = `Monto: ${anuncio.monto !== null ? `$${anuncio.monto}` : 'Negociable'}`;
-  
-        const fechaPublicacion = document.createElement('p');
-        fechaPublicacion.textContent = `Fecha de Publicación: ${anuncio.fechaPublicacion}`;
-  
-        const buttonsDiv = document.createElement('div');
-        buttonsDiv.className = 'anuncio-buttons';
-        
-        const editarButton = document.createElement('button');
-        editarButton.textContent = 'Editar';
-        
-        const eliminarButton = document.createElement('button');
-        eliminarButton.textContent = 'Eliminar';
-        
-        buttonsDiv.appendChild(editarButton);
-        buttonsDiv.appendChild(eliminarButton);
-        
-        li.appendChild(titulo);
-        li.appendChild(descripcion);
-        li.appendChild(categoria);
-        li.appendChild(ubicacion);
-        li.appendChild(monto);
-        li.appendChild(fechaPublicacion);
-        li.appendChild(buttonsDiv);
-        
-        anunciosList.appendChild(li);
+          const anuncioCard = crearAnuncioCard(anuncio);
+          anunciosList.appendChild(anuncioCard);
       });
-    }
-  
-    // Llamada inicial para obtener anuncios al cargar la página
-    obtenerAnuncios();
-  });
-  
+  }
+
+  // Crear un elemento de anuncio (li) con su contenido
+  function crearAnuncioCard(anuncio) {
+      const li = document.createElement('li');
+      li.className = 'anuncio-card';
+
+      const titulo = crearElemento('h3', 'anuncio-titulo', anuncio.titulo);
+      const descripcion = crearElemento('p', 'anuncio-descripcion', anuncio.descripcion);
+      const categoria = crearElemento('p', '', `Categoría: ${anuncio.categoria}`);
+      const ubicacion = crearElemento('p', '', `Ubicación: ${anuncio.ubicacion}`);
+      const monto = crearElemento('p', '', `Monto: ${formatoMonto(anuncio.monto)}`);
+      const fechaPublicacion = crearElemento('p', '', `Fecha de Publicación: ${formatoFecha(anuncio.fechaPublicacion)}`);
+
+      li.appendChild(titulo);
+      li.appendChild(descripcion);
+      li.appendChild(categoria);
+      li.appendChild(ubicacion);
+      li.appendChild(monto);
+      li.appendChild(fechaPublicacion);
+
+      return li;
+  }
+
+  // Formatear monto (si es nulo, mostrar "Negociable")
+  function formatoMonto(monto) {
+      return monto !== null ? `$${monto}` : 'Negociable';
+  }
+
+  function formatoFecha(fecha) {
+    const opcionesFecha = { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    return new Date(fecha).toLocaleDateString('es-ES', opcionesFecha);
+}
+
+  // Crear un elemento HTML
+  function crearElemento(tagName, className, texto) {
+      const elemento = document.createElement(tagName);
+      if (className) {
+          elemento.className = className;
+      }
+      elemento.textContent = texto;
+      return elemento;
+  }
+
+  // Obtener datos del formulario de anuncio
+  function obtenerDatosFormulario(form) {
+      const tipo = form.elements.tipo.value;
+      const titulo = form.elements.titulo.value;
+      const descripcion = form.elements.descripcion.value;
+      const categoria = form.elements.categoria.value;
+      const ubicacion = form.elements.ubicacion.value;
+      const monto = form.elements.monto.value === '' ? null : parseFloat(form.elements.monto.value);
+      const fechaActual = new Date();
+      const fechaPublicacion = fechaActual.toLocaleString(); // Fecha y hora actual
+
+      const nuevoAnuncio = {
+          tipo,
+          titulo,
+          descripcion,
+          categoria,
+          ubicacion,
+          monto,
+          fechaPublicacion,
+      };
+
+      return nuevoAnuncio;
+  }
+
+  // Agregar un nuevo anuncio al principio de la lista
+  function agregarAnuncioAlPrincipio(anuncio) {
+      const nuevoAnuncioCard = crearAnuncioCard(anuncio);
+      anunciosList.insertBefore(nuevoAnuncioCard, anunciosList.firstChild);
+  }
+
+  // Llamada inicial para obtener anuncios al cargar la página
+  obtenerAnuncios();
+});
