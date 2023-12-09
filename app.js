02dcb7f5a5b3bb92b90828b6fbed55d1b74be993
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-12-09 16:00:55 -0500

    eliminación de comentarios y creaciónd e variables de entorno para mongodb uri

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const adRoutes = require('./routes/adRoutes'); // Ruta a los archivos de rutas
-
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://JairoProDev:isOgCEALmpQsfA86@cluster0.cykdeq5.mongodb.net/?retryWrites=true&w=majority', {
+// Aplicar middleware CORS a todas las rutas
+app.use(cors());
+// Usa la variable de entorno MONGODB_URI o proporciona una cadena de conexión predeterminada
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://JairoProDev:isOgCEALmpQsfA86@cluster0.cykdeq5.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -18,44 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', adRoutes);
 
-/*
-app.use('/api', adRoutes); // Utiliza las rutas de anuncio
-
-const anuncioSchema = new mongoose.Schema({
-  adTitle: String,
-  descripcion: String,
-});
-
-const Anuncio = mongoose.model('Anuncio', anuncioSchema);
-
-app.use(express.json());
-
-app.post('/api/anuncios', async (req, res) => {
-  try {
-    const nuevoAnuncio = new Anuncio(req.body);
-    await nuevoAnuncio.save();
-    res.status(201).json({ mensaje: 'Anuncio creado exitosamente' });
-  } catch (error) {
-    res.status(500).json({ error: 'Error al crear el anuncio' });
-  }
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-/* app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-  }); */
-/*
-app.get('/api/anuncios', async (req, res) => {
-  try {
-    const anuncios = await Anuncio.find();
-    res.status(200).json(anuncios);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los anuncios' });
-  }
-});
-
-*/
 
 app.listen(PORT, () => {
   console.log(`Servidor en línea en http://localhost:${PORT}`);
