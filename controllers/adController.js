b3c03f3743b3e6ea48b54857ada99edd4c1b7028
Jairo Saul Salas Quiñones Pro-Dev <JairoProDev@gmail.com>
2023-12-10 19:52:17 -0500

    agregando logs  más especificos a la función o btener anuncios en adcontroller

--- controllers/adController.js
+++ controllers/adController.js
@@ -13,9 +13,15 @@ const crearAnuncio = async (req, res) => {
 const obtenerAnuncios = async (req, res) => {
   try {
     const anuncios = await Anuncio.find();
+
+    if (!anuncios || anuncios.length === 0) {
+      return res.status(404).json({ mensaje: 'No se encontraron anuncios' });
+    }
+
     res.status(200).json(anuncios);
   } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los anuncios' });
+    console.error('Error al obtener los anuncios:', error);
+    res.status(500).json({ error: 'Error interno al obtener los anuncios' });
   }
 };
 
