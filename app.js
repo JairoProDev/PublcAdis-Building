d5c2cb83eecdd09e355474ce24e89413836886ab
Jairo Saul <JairoProDev@gmail.com>
2024-01-31 14:40:32 -0500

    Update MongoDB connection with mongoose

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build'))); // Servir archivos estáticos
 
-// MongoDB connection
+// MongoDB connection with mongoose
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
