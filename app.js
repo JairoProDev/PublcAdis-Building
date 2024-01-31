b4526e6c399810932fdc887ffe1d6d13be8f31b5
Jairo Saul <JairoProDev@gmail.com>
2024-01-31 13:27:30 -0500

    Update static file path and add Heroku post-build script

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'client/build'))); // Servir archivos estáticos
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
@@ -30,6 +30,10 @@ app.use('/api', adRoutes);
 // Start server
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Error handling middleware
